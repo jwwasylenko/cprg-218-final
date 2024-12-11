9e581715e6f659103ae00ff547623da9082e1957
jwwasylenko <jwwasy@gmail.com>
2024-12-11 13:25:13 -0700

    Added Pages to HTML
    Added contact and about page. Still need to do attributions.

---
+++ portfolio.js
@@ -0,0 +1,49 @@
+mindfulicity.addEventListener('click',function (){
+    console.log("Project being changed");
+    document.getElementById("banner").src="images/Mindfulicity.png";
+    document.getElementById("CompanyName").innerHTML="Mindfulicity";
+    console.log("Project name being changed");
+    document.getElementById("anecdotes").innerHTML="Administered the Mindfulicity public profile, including the launch of their presence on LinkedIn and gained 200+ Followers in 4 months. Monitored trending conversations and audience insights on social networking websites. Analyzed performance of channels and campaigns and strategized to improve metrics.";
+    console.log("anecdotes changing");
+})
+
+accessiblerennovationsforkids.addEventListener('click',function (){
+    console.log("Project being changed");
+    document.getElementById("banner").src="images/Ashley.png";
+    document.getElementById("CompanyName").innerHTML="ARK (Accessible Rennovations for Kids";
+    console.log("Project name being changed");
+    document.getElementById("anecdotes").innerHTML="Example of my copywriting and social media posting skills with introducing a new ARK child. Produce content about ARK and related topics to attract more support and promote ARK brand.";
+    console.log("anecdotes changing");
+})
+
+heuristicshero.addEventListener('click',function (){
+    console.log("Project being changed");
+    document.getElementById("banner").src="images/duolingomockup.png";
+    document.getElementById("CompanyName").innerHTML="Heuristics Hero Award";
+    console.log("Project name being changed");
+    document.getElementById("anecdotes").innerHTML="I demonstrate skill in improving user experience through an understanding and application of heuristics principles. I made mockups for the language acqusition application named Duolingo. As a result of the frustration or pain point, have when faced with already opening a lesson and having auditory cues turned off. I can instead turn them off prior to entering the lesson itself. I added the black auditory button that you can turn off auditory lessons that are included in that lesson if the lesson contains a button. Having auditory cues be shut off within the lesson might frustrate users who are cramming language lessons into an already busy schedule.";
+    console.log("anecdotes changing");
+})
+
+marketingmaven.addEventListener('click',function (){
+    console.log("Project being changed");
+    document.getElementById("banner").src="images/LimTea.png";
+    document.getElementById("CompanyName").innerHTML="Marketing Maven Award";
+    console.log("Project name being changed");
+    document.getElementById("anecdotes").innerHTML="A commercial I helped create for LimTea, a fictional iced tea brand.";
+    console.log("anecdotes changing");
+})
+
+neuraurablogpost.addEventListener('click',function (){
+    console.log("Project being changed");
+    document.getElementById("banner").src="images/Neuraura.png";
+    document.getElementById("CompanyName").innerHTML="Neuraura";
+    console.log("Project name being changed");
+    document.getElementById("anecdotes").innerHTML="Created a 300-word blog post for the company Neuraura, to raise awareness about their product. The content was specifically targeted to women experiencing symptoms of PCOS (Polycystic ovary syndrome) who may be searching symptoms online.";
+    console.log("anecdotes changing");
+})
+
+
+
+
+
